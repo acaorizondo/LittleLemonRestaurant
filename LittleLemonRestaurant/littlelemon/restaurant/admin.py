a2b836d6184396d494c9547797a2b4b5222bfee8
Arturo Arias <acaorizondo@gmail.com>
2023-08-25 12:03:51 -0400

    Adding models

--- LittleLemonRestaurant/littlelemon/restaurant/admin.py
+++ LittleLemonRestaurant/littlelemon/restaurant/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Booking, Menu
 
 # Register your models here.
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('Title', 'Price', 'Inventory')
+@admin.register(Booking)    
+class BookingAdmin(admin.ModelAdmin):
+    list_display=('Name','No_of_guests','Booking_Date')
--- LittleLemonRestaurant/littlelemon/restaurant/models.py
+++ LittleLemonRestaurant/littlelemon/restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.SmallIntegerField()
+    Booking_Date = models.DateField()
+    
+    def __str__(self) -> str:
+        return self.Name
+    
+class Menu(models.Model):
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(decimal_places=2, max_digits=10)
+    Inventory = models.SmallIntegerField()
+    
+    def __str__(self) -> str:
+        return f"{self.Title}, {self.Price}, {self.Inventory}" 
