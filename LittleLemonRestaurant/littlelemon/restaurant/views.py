e801f52ac64afb17d1df48f8e0785f9a0da25b1c
Arturo Arias <acaorizondo@gmail.com>
2023-08-24 22:34:53 -0400

    basic configurations

--- LittleLemonRestaurant/littlelemon/restaurant/views.py
+++ LittleLemonRestaurant/littlelemon/restaurant/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from datetime import datetime
+# Create your views here.
 
 # Create your views here.
+def index(request):
+    current_year = datetime.now().year
+    context = {
+        'current_year': current_year
+    }
+    return render(request, 'index.html', context)
